0a6b86f7f575f3e2b17dd463cc2eceff1c692eb6
chetanraval07 <chetanravalcr03@gmail.com>
2026-01-13 19:13:13 +0530

    hello

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ terraform {
     bucket         = "jenkins-bucxx"
     key            = "infra/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
